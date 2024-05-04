e07856fe0dc2a93d222098387c29247d747612eb
EffahAdode3 <whatyousow00@gmail.com>
2024-05-04 14:06:11 -0700

    Four

--- phoneControls.js
+++ phoneControls.js
@@ -6,19 +6,8 @@ class PhoneControls{
 
     }
        addListeners(){
-        // window.addEventListener("deviceorientation", (e) =>  {
-        //     // console.log(e);
-        //     this.tilt = e.beta * Math.PI /180 ;
-        //     const canvasAngle = -this.tilt;
-        //     this.canvas.style.transform = 
-        //     "translate(-50%, -50%) rotate(" + canvasAngle + "rad)";
-        // });
-        window.addEventListener("devicemotion", (e) =>  {
+        window.addEventListener("deviceorientation", (e) =>  {
             // console.log(e);
-            this.tilt = Math.atan2(
-                e.accelerationIncludingGravity.y,
-                e.accelerationIncludingGravity.x
-            )
             this.tilt = e.beta * Math.PI /180 ;
             const canvasAngle = -this.tilt;
             this.canvas.style.transform = 
