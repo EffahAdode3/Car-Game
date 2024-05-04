342a0df326a2ab7fe11e14726f3d79996f828030
EffahAdode3 <whatyousow00@gmail.com>
2024-05-04 04:57:59 -0700

    first commit

---
+++ phoneControls.js
@@ -0,0 +1,18 @@
+
+class PhoneControls{
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.tilt = 0;
+        this.#addListeners();
+
+    }
+    #addListeners(){
+        window.addEventListener("deviceorientation", (e) =>  {
+            // console.log(e);
+            this.tilt = e.beta * Math.PI /180 ;
+            const canvasAngle = -this.tilt;
+            this.canvas.style.transform = 
+            "translate(-50%, -50%) rotate(" + canvasAngle + "rad)";
+        });
+    }
+}
