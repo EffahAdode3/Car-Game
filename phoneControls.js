864ba2cd14baad1ef53e7b5ad81fd1ab064b8880
EffahAdode3 <whatyousow00@gmail.com>
2024-05-04 13:19:20 -0700

    Third Commit

--- phoneControls.js
+++ phoneControls.js
@@ -1,4 +1,3 @@
-
 class PhoneControls{
     constructor(canvas) {
         this.canvas = canvas;
@@ -7,8 +6,19 @@ class PhoneControls{
 
     }
        addListeners(){
-        window.addEventListener("deviceorientation", (e) =>  {
+        // window.addEventListener("deviceorientation", (e) =>  {
+        //     // console.log(e);
+        //     this.tilt = e.beta * Math.PI /180 ;
+        //     const canvasAngle = -this.tilt;
+        //     this.canvas.style.transform = 
+        //     "translate(-50%, -50%) rotate(" + canvasAngle + "rad)";
+        // });
+        window.addEventListener("devicemotion", (e) =>  {
             // console.log(e);
+            this.tilt = Math.atan2(
+                e.accelerationIncludingGravity.y,
+                e.accelerationIncludingGravity.x
+            )
             this.tilt = e.beta * Math.PI /180 ;
             const canvasAngle = -this.tilt;
             this.canvas.style.transform = 
